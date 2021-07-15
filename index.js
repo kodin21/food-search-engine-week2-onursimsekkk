e2a23478c63d87a2ed488dbee60d1b3da0213bdf
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-15 20:24:54 +0300

    Add loadFoodItems func

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const root = document.querySelector('#js-root');
 const user = document.getElementById('user-info');
 
+// Fetch User Info
 fetch("https://jsonplaceholder.typicode.com/users/1").then(
   response => response.json()
   ).then(responseJson => {
@@ -10,4 +11,44 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then(
     user.appendChild(userName);
   });
 
+  // const searchArea = document.getElementById('search');
+  const foodArea = document.getElementById('food-box');
 
+  function loadFoodItems() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(responseJson => {
+      responseJson.forEach(food => {
+        const foodCard = document.createElement('div');
+        foodCard.innerHTML = food.title;
+        foodCard.className = "foodCard";
+
+        const favBtn = document.createElement('button');
+        favBtn.setAttribute('id', food.id);
+        favBtn.classList.add(
+          "favBtn",
+          "p-2",
+          "btn",
+          "btn-primary"
+        )
+        favBtn.textContent = "FAV"
+        foodCard.appendChild(favBtn);
+        foodArea.appendChild(foodCard);
+      });
+    });
+  };
+
+  loadFoodItems();
+  // function newElement() {
+  //   const newEl = document.createElement('button');
+  //   newEl.textContent = "Bana Tıkla";
+  //   newEl.setAttribute('id', 'sendBtn');
+  //   searchArea.appendChild(newEl);
+  // };
+  // newElement();
+  
+  // document.addEventListener('click', (e) => {
+  //   if(e.target.id === "sendBtn") {
+  //     console.log("Butona tıklandı");
+  //   }
+  // });
