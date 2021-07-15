cdaef7c44b3ca990d1be4423296f5c79a278bcbe
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-15 19:11:20 +0300

    Add user fetch

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('test')
+const root = document.querySelector('#js-root');
+const user = document.getElementById('user-info');
+
+fetch("https://jsonplaceholder.typicode.com/users/1").then(
+  response => response.json()
+  ).then(responseJson => {
+    const userName = document.createElement('div');
+    userName.textContent = `Hello, ${responseJson.name}`;
+    userName.setAttribute('id', 'userName');
+    user.appendChild(userName);
+  });
+
+
