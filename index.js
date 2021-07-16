6af4a16d3c5db89c266cb3b59843e28f1dd5c7fa
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-16 14:59:37 +0300

    Add focus to card func

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const root = document.querySelector('#js-root');
+// const root = document.querySelector('#js-root');
 const user = document.getElementById('user-info');
 
 // Fetch User Info
 fetch("https://jsonplaceholder.typicode.com/users/1").then(
   response => response.json()
-  ).then(responseJson => {
+  ).then(data => {
     const userName = document.createElement('div');
-    userName.textContent = `Hello, ${responseJson.name}`;
+    userName.textContent = `Hello, ${data.name}`;
     userName.setAttribute('id', 'userName');
     user.appendChild(userName);
   });
 
-  // const searchArea = document.getElementById('search');
   const foodArea = document.getElementById('food-box');
 
   function loadFoodItems() {
     fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
-    .then(responseJson => {
-      responseJson.forEach(food => {
+    .then(data => {
+      data.forEach(food => {
         const foodCard = document.createElement('div');
         foodCard.innerHTML = food.title;
         foodCard.className = "foodCard";
@@ -31,7 +30,7 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then(
           "btn",
           "btn-primary"
         )
-        favBtn.textContent = "FAV"
+        favBtn.textContent = "FAV";
         foodCard.appendChild(favBtn);
         foodArea.appendChild(foodCard);
       });
@@ -39,16 +38,27 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then(
   };
 
   loadFoodItems();
-  // function newElement() {
-  //   const newEl = document.createElement('button');
-  //   newEl.textContent = "Bana Tıkla";
-  //   newEl.setAttribute('id', 'sendBtn');
-  //   searchArea.appendChild(newEl);
-  // };
-  // newElement();
-  
-  // document.addEventListener('click', (e) => {
-  //   if(e.target.id === "sendBtn") {
-  //     console.log("Butona tıklandı");
+
+  const foodBox = document.getElementsByClassName("food-box");
+  // const foodCard = document.querySelector('.foodCard');
+  document.addEventListener('click', focusFoodCard);
+  document.addEventListener('mouseout', leaveFoodCard);
+
+  // Focus to card Function
+  function focusFoodCard(e) {
+    if(e.target.className === "foodCard") {
+      e.target.classList.add("borderEffect", "bg-info");
+    }
+  };
+  // // Focus Out function
+  // function leaveFoodCard(e) {
+  //   if(e.target.classList.contains("borderEffect")) {
+  //     e.target.classList.remove("borderEffect", "bg-info");
   //   }
-  // });
+  // };
+
+  // const searchInput = document.getElementById('searchInput');
+
+  // searchInput.addEventListener('focusin', (e) => {
+  //   e.target.style.background = "red";
+  // })
